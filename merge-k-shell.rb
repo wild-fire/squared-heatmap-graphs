68ed351f69b3d440a0c13fe4098a0701417722ff
David Brenes <davidjbrenes@gmail.com>
2015-11-13 22:44:16 +0100

    New pagerank data, some changes in controllability seed and header for CSVs

--- merge-k-shell.rb
+++ merge-k-shell.rb
@@ -2,18 +2,38 @@ require 'csv'
 
 controllability = {}
 k_shell = {}
+pagerank = {}
 
-CSV.foreach('controlabilidad.csv', headers: true).each do |row|
+CSV.foreach('data/controlabilidad.csv', headers: true).each do |row|
   controllability[row["nodo"]] = row["controlabilidad"]
 end
 
-CSV.foreach('kshell.csv', headers: true).each do |row|
+CSV.foreach('data/kshell.csv', headers: true).each do |row|
   k_shell[row["nodo"]] = row["k-shell"]
 end
 
-CSV.open('k-shell-controlabilidad.csv', 'w') do |csv|
+CSV.foreach('data/pagerank.csv', headers: true).each do |row|
+  pagerank[row["nodo"]] = row["pagerank"]
+end
+
+CSV.open('data/k-shell-controlabilidad.csv', 'w') do |csv|
+
+  csv << ["nodo", "controlability", "kshell"]
 
   controllability.each do |node, score|
+    if k_shell[node]
+      csv << [node, score, k_shell[node]]
+    end
+  end
+
+end
+
+
+CSV.open('data/k-shell-pagerank.csv', 'w') do |csv|
+
+  csv << ["nodo", "pagerank", "kshell"]
+
+  pagerank.each do |node, score|
     if k_shell[node]
       csv << [node, score, k_shell[node]]
     end
