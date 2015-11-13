bba6a6f85f58c10920cb1e06e2396d9206cc5a91
David Brenes <davidjbrenes@gmail.com>
2015-11-13 22:44:54 +0100

    Using correct folders for shell-controllability heatmaps

--- shell-controlability-heatmap.rb
+++ shell-controlability-heatmap.rb
@@ -33,7 +33,7 @@ by_controlability_heatmap = {}
 max_kshell = 9
 alpha = 0.01
 
-CSV.foreach('k-shell-controlabilidad.csv', headers: true).each do |row|
+CSV.foreach('data/k-shell-controlabilidad.csv', headers: true).each do |row|
   kshell = row['kshell'].to_f
   controlability = row['controlability'].to_f
   controllability_index = (controlability*10).to_i
@@ -46,5 +46,5 @@ CSV.foreach('k-shell-controlabilidad.csv', headers: true).each do |row|
   by_controlability_heatmap[controllability_index][kshell-2] += 1
 end
 
-create_graphic "shell-controllability-by-shell.jpg", by_shell_heatmap, 8, 10
-create_graphic "shell-controllability-by-controllability.jpg", by_controlability_heatmap, 10, 8
+create_graphic "heatmaps/shell-controllability-by-shell.jpg", by_shell_heatmap, 8, 10
+create_graphic "heatmaps/shell-controllability-by-controllability.jpg", by_controlability_heatmap, 10, 8
